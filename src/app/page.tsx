1dc0d224465295b78712be07ab9ce701da7b5f1e
roguecodes <you@example.com>
2025-08-10 14:30:53 +0530

    Add authentication functionality and update dependencies
    - Implemented authentication using better-auth with email and password support.
    - Created auth client and route for handling authentication requests.
    - Updated package.json and package-lock.json to include better-auth and related dependencies.
    - Defined user, session, account, and verification tables in the database schema.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,102 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
-  return <Button>Install</Button>;
+  const { data: session } = authClient.useSession();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    pass: "",
+  });
+
+  const handleChange = (name: string, value: string) => {
+    setForm((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  };
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email: form.email,
+        name: form.name,
+        password: form.pass,
+      },
+      {
+        onError: () => {
+          window.alert("something went wrong!");
+        },
+        onSuccess: () => {
+          window.alert("sucess");
+        },
+      }
+    );
+  };
+  const onSignin = () => {
+    authClient.signIn.email(
+      {
+        email: form.email,
+        password: form.pass,
+      },
+      {
+        onError: () => {
+          window.alert("something went wrong!");
+        },
+        onSuccess: () => {
+          window.alert("sucess");
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session?.user?.name}</p>
+        <Button onClick={() => authClient?.signOut()}> Sign out</Button>
+      </div>
+    );
+  }
+  return (
+    <>
+      <div className="p-4 w-1/5 flex flex-col gap-y-4">
+        <Input
+          placeholder="name"
+          value={form.name}
+          onChange={(e) => handleChange("name", e.target.value)}
+        />
+        <Input
+          placeholder="email"
+          value={form.email}
+          onChange={(e) => handleChange("email", e.target.value)}
+        />
+        <Input
+          placeholder="pass"
+          value={form.pass}
+          onChange={(e) => handleChange("pass", e.target.value)}
+        />
+        <Button onClick={onSubmit}>Create User</Button>
+      </div>
+      <div className="p-4 w-1/5 flex flex-col gap-y-4">
+        <Input
+          placeholder="email"
+          value={form.email}
+          onChange={(e) => handleChange("email", e.target.value)}
+        />
+        <Input
+          placeholder="pass"
+          value={form.pass}
+          onChange={(e) => handleChange("pass", e.target.value)}
+        />
+        <Button onClick={onSignin}>Sign in</Button>
+      </div>
+    </>
+  );
 }
